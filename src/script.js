4aba62a9fef5cae1348fa6199c6c7964177fa2ae
Dree20 <dreannaad@gmail.com>
2025-08-19 17:21:03 -0400

    Updated Html and Added JS
    Added Id's to Html and added basic functions to JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-
+function newLanguage(event) {
+  event.preventDefault();
+  new Typewriter("#new-word", {
+    strings: "Como vai voce?",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let formElement = document.querySelector("#language-form");
+formElement.addEventListener("submit", newLanguage);
